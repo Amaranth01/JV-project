a7c6cb21619bfd31df727f813fa20c99d6473e8d
Amaranth01 <vanessa.nectoux@gmail.com>
2022-02-10 10:53:11 +0100

    début du home

--- public/home.php
+++ public/home.php
@@ -5,3 +5,20 @@ require __DIR__ . '/../partials/footer.php';
 
 ?>
 
+<div>
+    <h2>Nos derniers articles</h2>
+</div>
+
+<div>
+    <h2>Nos derniers tests</h2>
+</div>
+
+<div>
+    <h2>Nos dernières séries</h2>
+</div>
+
+<div>
+    <h2>Les jeux les plus attendus</h2>
+        <h3>Par vous</h3>
+        <h3>Par la rédaction</h3>
+</div>
