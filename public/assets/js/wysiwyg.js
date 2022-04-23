c2c9526efcb5a89eb1ab0fdd03d04dcad4b0085c
Amaranth01 <vanessa.nectoux@gmail.com>
2022-04-23 15:44:41 +0200

    finish wysiwyg

--- public/assets/js/wysiwyg.js
+++ public/assets/js/wysiwyg.js
@@ -7,8 +7,13 @@ tinymce.init({
         'searchreplace', 'wordcount', 'visualblocks', 'visualchars', 'fullscreen', 'insertdatetime',
         'media', 'table', 'help'
     ],
+    //toolbar customization
     toolbar: 'insertfile undo redo | styles | bold italic visualblocks| alignleft aligncenter alignright alignjustify | ' +
         'bullist numlist | link image | preview | forecolor backcolor emoticons | fullscreen',
 
+    //defines the allowed format
+    images_file_types:'jpeg,jpg,jfif,png,gif,webp',
+
+    link_default_target: '_blank'
 });
 
