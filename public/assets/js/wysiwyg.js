674950a93b03d30c8c3242aaf721d92d43b87701
Amaranth01 <vanessa.nectoux@gmail.com>
2022-04-23 15:19:31 +0200

    Customization of wysiwyg

--- public/assets/js/wysiwyg.js
+++ public/assets/js/wysiwyg.js
@@ -1,4 +1,14 @@
 tinymce.init({
-    selector: 'textarea#editor'
+    selector: 'textarea#editor',
+    height: 700,
+    //Select plugins to include on load
+    plugins: [
+        'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'preview', 'anchor', 'pagebreak',
+        'searchreplace', 'wordcount', 'visualblocks', 'visualchars', 'fullscreen', 'insertdatetime',
+        'media', 'table', 'help'
+    ],
+    toolbar: 'insertfile undo redo | styles | bold italic visualblocks| alignleft aligncenter alignright alignjustify | ' +
+        'bullist numlist | link image | preview | forecolor backcolor emoticons | fullscreen',
+
 });
 
