4a897f919f2367fb5b797fea41bf2529f5bd9b53
Amaranth01 <vanessa.nectoux@gmail.com>
2022-05-20 16:25:12 +0200

    add responsive menu

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,5 +3,17 @@ setTimeout(() => {
     document.querySelectorAll('.message').forEach(error => error.remove());
 }, 6000);
 
+//show responsive menu
 let menu = document.querySelector('.burger');
+let element = document.querySelector('.displayLink');
+let a = 0;
 
+menu.addEventListener('click', function () {
+    if(a === 0) {
+        element.style.display = 'block';
+        a++
+    } else if(a === 1  ){
+        element.style.display = 'none';
+        a--
+    }
+});
